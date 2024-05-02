528bd610ac70b6850c2542af0a030c84b4e6d28e
Aruzhan Jankushukova <a.dzhankushukova@ticketon.kz>
2024-05-02 20:20:47 +0500

    init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+const (
+	gauge   = "gauge"
+	counter = "counter"
+)
+
+func main() {
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc(`/update/{type}/{name}/{value}`, handler)
+
+	return http.ListenAndServe(`:8080`, mux)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+}
