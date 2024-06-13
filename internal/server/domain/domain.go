b17c1b95ea678987efd1dfd18b5dfe41ece62172
Aruzhan Jankushukova <a.dzhankushukova@ticketon.kz>
2024-06-13 15:53:30 +0500

    iter 5 changes

--- internal/server/domain/domain.go
+++ internal/server/domain/domain.go
@@ -1,19 +1,19 @@
 package domain
 
-// MetricRequest is a request struct for services.service.update
+// MetricRequest is a request struct for metrics
 type MetricRequest struct {
 	Type  string  `json:"type"`
 	Name  string  `json:"name"`
 	Value float64 `json:"value"`
 }
 
-// UpdateResponse is a response struct for services.service.update
+// UpdateResponse is a response struct for metrics update
 type UpdateResponse struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
 
-// MetricResponse is a response struct
+// MetricResponse is a response struct for metrics
 type MetricResponse struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
